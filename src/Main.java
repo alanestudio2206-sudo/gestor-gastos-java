069ef460d02f3c6b97c15bf7180ae2bd800de8b4
abrena09-PIC <abrena09@alumnos.uaq.mx>
2026-08-24 21:53:52 -0600

    Agregué cálculos total y gasto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Gestor semanal de Gastos");
     }
+
+    public static double calcularTotal(ArrayList<Double> montos) {
+        double total = 0;
+        for (double m : montos) {
+            total += m;
+        }
+        return total;
+    }
+
+    public static int obtenerPosicionGastoMayor(ArrayList<Double> montos) {
+        if (montos.isEmpty()) {
+            return -1;
+        }
+        int posicionMayor = 0;
+        for (int i = 1; i < montos.size(); i++) {
+            if (montos.get(i) > montos.get(posicionMayor)) {
+                posicionMayor = i;
+            }
+        }
+        return posicionMayor;
+    }
+
 }
